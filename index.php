7fe314b9d1a6508bae87b33b997857e4beec5198
Paola <lehhsilva25122023@gmail.com>
2025-10-23 13:12:02 -0300

    criação da página inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>To_do_List</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>
+            Minha To_do_List 
+        </h1>
+    <form action="add_task.php" method="POST">
+        <input type="text" name="title" placeholder="nova tarefa..." required>
+        <button type="submite"> Adicionar </button>
+    </form>
+    </body>
+</html>
