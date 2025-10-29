ab11e5ebae556713d8fb725b77d220a4e6384d09
Paola <lehhsilva25122023@gmail.com>
2025-10-29 10:31:17 -0300

    criação de estilos da página inicial

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
+<?php include ('db_connection.php'); ?>
 
+<!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
@@ -14,5 +15,27 @@
         <input type="text" name="title" placeholder="nova tarefa..." required>
         <button type="submite"> Adicionar </button>
     </form>
-    </body>
+    <hr>
+    <ul>
+        <?php
+        $sql = "SELECT * FROM tasks ORDER BY id DESC";
+        $result = $conn->query(query: $sql);
+        if ($result->num_rows > 0){
+            while ($row = $result->fetch_assoc()){
+            echo "<lie>";
+            echo $row ['status'] == 'cncluida' ? "<s> {$row ['tittle']} </s>": $row ['tittle'];
+            echo"
+            <a href = 'update_task.php?id={$row['id']}'>V</a>
+            <a href = 'delete_task.php?id={$row['id']}'>L</a>";
+            echo "</li>";
+
+        }
+    }else{
+        echo "<p> Nenhuma tarefa cadastrada.</php>";
+
+    }
+
+    ?>
+    </ul>
+</body>
 </html>
